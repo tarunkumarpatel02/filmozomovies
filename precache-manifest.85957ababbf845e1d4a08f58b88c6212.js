1f9a597084e767521a465584149c498673ab2d80
tarunkumarpatel02 <tarunkpatel02@gmail.com>
2020-05-04 18:04:02 +0530

    Updates

---
+++ precache-manifest.85957ababbf845e1d4a08f58b88c6212.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea5da9ac4b4b43fe52e0baaf016179ba",
+    "url": "/filmozomovies/index.html"
+  },
+  {
+    "revision": "58bf9c4f831b56f88a2d",
+    "url": "/filmozomovies/static/css/2.fddaa3a5.chunk.css"
+  },
+  {
+    "revision": "34fbad39547dc1aa1c93",
+    "url": "/filmozomovies/static/css/main.244ffb73.chunk.css"
+  },
+  {
+    "revision": "58bf9c4f831b56f88a2d",
+    "url": "/filmozomovies/static/js/2.7413d510.chunk.js"
+  },
+  {
+    "revision": "00f89bfdd2b8ccea7cfd359d69e2f3b7",
+    "url": "/filmozomovies/static/js/2.7413d510.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34fbad39547dc1aa1c93",
+    "url": "/filmozomovies/static/js/main.32f9776b.chunk.js"
+  },
+  {
+    "revision": "0177c401b34e12f790d5",
+    "url": "/filmozomovies/static/js/runtime-main.5338f450.js"
+  },
+  {
+    "revision": "f883bd61859350cdcafa4680183745ea",
+    "url": "/filmozomovies/static/media/circle-cropped.f883bd61.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7c1fc9f112b8794747374ea7d80539f3.js"
+  "/filmozomovies/precache-manifest.85957ababbf845e1d4a08f58b88c6212.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/filmozomovies/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
